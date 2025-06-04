db66679c2406ab08c9dc936462427f0c1da60c8e
plunkydev <plunkydesign@gmail.com>
2025-06-04 13:44:41 -0500

    feat: integrar UI de filas con botones para agregar y quitar elementos en el DOM

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 import './style.css'
-import { queue } from '/src/dataTypes/queue'
-console.log(queue);
+import { filasUI, addFila, removeFila } from '/src/components/filas'
+
+const dataContainer = document.getElementById('dataContainer');
+const dataBtnFilas = document.getElementById('dataBtnFilas');
+
+dataBtnFilas.addEventListener('click', () => {
+    dataContainer.innerHTML = filasUI();
+    document.getElementById('addcola').addEventListener('click', addFila);
+    document.getElementById('removeCola').addEventListener('click', removeFila);
+})
