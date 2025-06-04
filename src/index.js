a459cd74d3118df503916ea8f9e310beea8d41a6
plunkydev <plunkydesign@gmail.com>
2025-06-04 17:58:49 -0500

    feat: integrar UI de pila y agregar botón para renderizar su estructura en el contenedor principal

--- src/index.js
+++ src/index.js
@@ -1,11 +1,17 @@
 import './style.css'
 import { filasUI, addFila, removeFila } from '/src/components/filas'
+import { pilasUI } from '/src/components/pilas'
 
 const dataContainer = document.getElementById('dataContainer');
 const dataBtnFilas = document.getElementById('dataBtnFilas');
+const dataBtnPila = document.getElementById('dataBtnPila');
 
 dataBtnFilas.addEventListener('click', () => {
     dataContainer.innerHTML = filasUI();
     document.getElementById('addcola').addEventListener('click', addFila);
     document.getElementById('removeCola').addEventListener('click', removeFila);
-})
+});
+
+dataBtnPila.addEventListener('click', () => {
+    dataContainer.innerHTML = pilasUI();
+});
