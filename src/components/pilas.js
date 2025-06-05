daa207a22d99cd7f57fac2b968c8e8e37169ba91
plunkydev <plunkydesign@gmail.com>
2025-06-05 14:05:28 -0500

    feat: se integra la lógica de agregar y quitar elementos de la pila con interacción desde el DOM; se importa el módulo 'stak'; se agrega input numérico; se implementan funciones addPila y removePila con animaciones y validaciones

--- src/components/pilas.js
+++ src/components/pilas.js
@@ -1,3 +1,4 @@
+import { stak } from '/src/dataTypes/stak.js'
 const pilasUI = () => {
     let pilaContainer = `
         <div class="pilaContainer" id="pilaContainer">
@@ -8,6 +9,10 @@ const pilasUI = () => {
             <div class="uiPilaContainer" id="uiPilaContainer">
                 <div class="pilaItemContainer" id="pilaItemContainer"></div>
                 <div class="uiPila" id="uiPila">
+                    <label for="pila">
+                        Pila
+                        <input type="number" placeholder="Numero" id="pila" name="pila">
+                    </label>
                     <button class="dataBtn" type="button" id="addPila">Agregar Pila</button>
                     <button class="dataBtn" type="button" id="removePila">Quitar de la Pila</button>
                 </div>
@@ -17,4 +22,53 @@ const pilasUI = () => {
     return pilaContainer
 }
 
-export { pilasUI }
+const addPila = () => {
+    if(document.getElementById('pila').value === '') return;
+    if(document.getElementById('pilaItemContainer').children.length  === 5) {
+        alert('Pila llena. maximo 5 elementos para representar este ejemplo');
+        return;
+    }
+    const pilaItemContainer = document.getElementById('pilaItemContainer');
+    const valorPila = document.getElementById('pila').value;
+
+    stak.push(valorPila);
+
+    const addPila = document.createElement('span');
+    addPila.classList.add('pila', 'fade-lento', 'oculto');
+    addPila.innerHTML = `${stak.peek()}`;
+
+    pilaItemContainer.appendChild(addPila);
+
+    void addPila.offsetWidth;
+
+    addPila.classList.remove('oculto');
+
+    document.getElementById('pila').value = '';
+    console.log(stak.print());
+}
+
+
+const removePila = () => {
+    if (stak.isEmpty()) return;
+
+    const pilaItemContainer = document.getElementById('pilaItemContainer');
+
+    const ultimoItem = pilaItemContainer.lastChild;
+    if (!ultimoItem) return;
+
+    ultimoItem.classList.add('fade-lento');
+
+    void ultimoItem.offsetWidth;
+    ultimoItem.classList.add('oculto');
+
+    setTimeout(() => {
+        stak.pop();
+        pilaItemContainer.removeChild(ultimoItem);
+        console.log(stak.print());
+    }, 500);
+
+    document.getElementById('pila').value = '';
+    
+}
+
+export { pilasUI, addPila, removePila }
