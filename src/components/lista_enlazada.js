57f4ffcf1d76ef2a26e29f464cfd5cd723d7cd42
plunkydev <plunkydesign@gmail.com>
2025-06-13 19:54:48 -0500

    feat(ui): crea componente listaEnlazadaUi con estructura HTML para agregar y eliminar nodos

---
+++ src/components/lista_enlazada.js
@@ -0,0 +1,35 @@
+const listaEnlazadaUi = () => {
+    let listaEnlazadaContainer = `
+        <div class="linkedLisContainer" id="linkedListContainer">
+            <div class="linkedListInfo">
+                <h2>Lista Enlazada (Linked List)</h2>
+                <p>Una lista enlazada es una colección **<strong>linear</strong>** constutuida por una secuencia de "nodos en donde se guardan campos de datos arbitrarios y una referencia al siguiente nodo.</p>
+            </div>
+            <div class="uiLinkedListContainer">
+                <div class="uiLinkedList">
+                    <div class="addNodoContainer">
+                        <label for="addNodoValue">Agregar dato
+                            <input class="listInput" type="number" placeholder="Numero" id="addNodoValue">
+                        </label>
+                        <button class="dataBtn" type="button">Agregar</button>
+                    </div>
+                    <div class="removeNodoContainer">
+                        <label for="removeNodoValue">Eliminar dato
+                            <input class="listInput" type="number" placeholder="en lista" id="removeNodoValue">
+                        </label>
+                        <button class="dataBtn" type="button">Eliminar</button>
+                    </div>
+                </div>
+                <div class="linkedListItemContainer border" id="linkedListItemContainer">
+                    <div class="listItem" id="listItem">
+                        <div class="data" id="data">1</div>
+                        <div class="next" id="next">ref</div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    `
+    return listaEnlazadaContainer
+}
+
+export { listaEnlazadaUi }
