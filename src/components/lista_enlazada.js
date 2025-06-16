11392d655e52e5cd84b869322b09b6cf69d42b36
plunkydev <plunkydesign@gmail.com>
2025-06-16 15:43:24 -0500

    feat(linked-list): integra estructura real y UI dinámica
    * Importa 'list' desde /src/dataTypes/linked-list.js y conecta lógica de datos
    
    * Añade contenedor de consola para imprimir estado de la lista
    
    * Implementa addNodo y removeNodo con lógica, animación y feedback visual

--- src/components/lista_enlazada.js
+++ src/components/lista_enlazada.js
@@ -1,3 +1,5 @@
+import { node } from 'webpack';
+import { list } from '/src/dataTypes/linked-list.js';
 const listaEnlazadaUi = () => {
     let listaEnlazadaContainer = `
         <div class="linkedLisContainer" id="linkedListContainer">
@@ -20,16 +22,56 @@ const listaEnlazadaUi = () => {
                         <button class="dataBtn" id="btnRemoveNodo" type="button">Eliminar</button>
                     </div>
                 </div>
-                <div class="linkedListItemContainer border" id="linkedListItemContainer">
-                    <div class="listItem" id="listItem">
-                        <div class="data" id="data">1</div>
-                        <div class="next" id="next">ref</div>
-                    </div>
+                <div class="listCosoleContainer border" id="listCosoleContainer">
+                    <p id="listConsole">Consola</p>
                 </div>
+                <div class="linkedListItemContainer border" id="linkedListItemContainer"></div>
             </div>
         </div>
     `
     return listaEnlazadaContainer
 }
 
-export { listaEnlazadaUi }
+const linkedListItem = (data) => {
+    return `<div class="data" id="data${data}">${data}</div>
+        <div class="next" id="next">ref</div>`
+}
+
+const addNodo = () => {
+    const nodeContainer = document.getElementById('linkedListItemContainer')
+    list.add(document.getElementById('addNodoValue').value)
+    const nodo = document.createElement('div')
+    nodo.classList.add('listItem','fade-lento', 'oculto');
+    nodo.id = `listItem${document.getElementById('addNodoValue').value}`
+    nodo.innerHTML = linkedListItem(document.getElementById('addNodoValue').value)
+    nodeContainer.appendChild(nodo)
+    void nodo.offsetWidth;
+    nodo.classList.remove('oculto');
+    document.getElementById('addNodoValue').value = ''
+    document.getElementById('listConsole').innerHTML = list.print()
+}
+const removeNodo = () => {
+    if(!list.size) {
+        document.getElementById('listConsole').innerHTML = "<p style='color: red;'>No hay datos en la lista</p>"
+        setTimeout(() => {
+            document.getElementById('listConsole').innerHTML = 'Consola'
+        }, 4000)
+    }
+    const nodeContainer = document.getElementById('linkedListItemContainer')
+    const nodeToRemove = document.getElementById('removeNodoValue').value
+    list.remove(nodeToRemove)
+    const nodeRemoved = document.getElementById(`listItem${nodeToRemove}`)
+    nodeRemoved.classList.add('fade-lento')
+    void nodeRemoved.offsetWidth;
+    nodeRemoved.classList.add('oculto');
+
+    setTimeout(() => {
+        nodeContainer.removeChild(nodeRemoved)
+        document.getElementById('listConsole').innerHTML = list.print()
+    }, 500);
+
+
+    document.getElementById('removeNodoValue').value = ''
+}
+
+export { listaEnlazadaUi, addNodo, removeNodo }
